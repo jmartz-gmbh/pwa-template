4e9739935e7a2b51a50b5ef3ba4e5207029208ef
Jonathan Martz <git@jmartz.de>
2022-06-03 12:00:46 +0200

    fix impressum link

--- app/src/components/footer.vue
+++ app/src/components/footer.vue
@@ -2,7 +2,7 @@
   <div class="vc-footer">
     <div class="grid grid-cols-8">
       <div class="col-span-8 md:col-span-2">
-        <router-link to="/ipmressum" class="text-blue-400">Impressum</router-link>
+        <router-link to="/impressum" class="text-blue-400">Impressum</router-link>
       </div>
       <div class="col-span-8 md:col-span-2"></div>
       <div class="col-span-8 md:col-span-2"></div>
