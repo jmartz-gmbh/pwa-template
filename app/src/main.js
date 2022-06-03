49430c1fa0d742046ef2c837c4c38c0169e3e189
Jonathan Martz <git@jmartz.de>
2022-06-03 12:12:28 +0200

    add plaubile tracking

--- app/src/main.js
+++ app/src/main.js
@@ -3,8 +3,18 @@ import App from './App.vue'
 import store from './store';
 import routes from './routes';
 
+import plausible from './plugins/plausible';
+
+const plausibleOptions = { 
+  domain: 'www.pwa-template.de',
+  apiHost: 'https://tracking.jmartz.gmbh',
+  hashMode: false,
+  trackLocalhost: false,
+};
+
 const app = createApp(App);
 
+app.use(plausible,plausibleOptions);
 app.use(routes);
 app.use(store);
 app.mount('#app')
