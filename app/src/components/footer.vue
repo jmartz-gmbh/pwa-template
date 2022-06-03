3095200ce83bc440ecfa86e0e9f9612d24b447aa
Jonathan Martz <git@jmartz.de>
2022-06-03 12:04:59 +0200

    update header footer and title

--- app/src/components/footer.vue
+++ app/src/components/footer.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="vc-footer">
-    <div class="grid grid-cols-8">
+    <div class="grid grid-cols-8 text-white">
       <div class="col-span-8 md:col-span-2">
-        <router-link to="/impressum" class="text-blue-400">Impressum</router-link>
+        <router-link to="/impressum" class="font-bold">Impressum</router-link>
       </div>
       <div class="col-span-8 md:col-span-2"></div>
       <div class="col-span-8 md:col-span-2"></div>
       <div class="col-span-8 md:col-span-2">
         Copyright by
-        <a href="http://www.jmartz.gmbh/" class="text-blue-400" target="_blank">Jmartz GmbH</a>
+        <a href="http://www.jmartz.gmbh/" class="font-bold" target="_blank">Jmartz GmbH</a>
       </div>
     </div>
   </div>
