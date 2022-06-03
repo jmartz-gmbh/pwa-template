6868a7b803666da7d918a220e233af5d6efe97d4
Jonathan Martz <git@jmartz.de>
2022-06-03 11:56:23 +0200

    fix logo path

--- app/src/pages/home.vue
+++ app/src/pages/home.vue
@@ -1,5 +1,5 @@
 <template>
   <div class="vp-home">
-    <img alt="Vue logo" src="./assets/logo.png" /> <welcome></welcome>
+    <img alt="Vue logo" src="../assets/logo.png" /> <welcome></welcome>
   </div>
 </template>
