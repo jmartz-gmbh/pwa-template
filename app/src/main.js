1bfbab3059f78809e3af8b9b395901e9d56b5eb0
Jonathan Martz <git@jmartz.de>
2022-06-13 15:00:17 +0200

    add font awesome

--- app/src/main.js
+++ app/src/main.js
@@ -1,20 +1,32 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import store from './store';
-import routes from './routes';
+import { createApp } from "vue";
+import App from "./App.vue";
+import store from "./store";
+import routes from "./routes";
 
-import plausible from './plugins/plausible';
+/* import the fontawesome core */
+import { library } from "@fortawesome/fontawesome-svg-core";
 
-const plausibleOptions = { 
-  domain: 'www.pwa-template.de',
-  apiHost: 'https://tracking.jmartz.gmbh',
+/* import specific icons */
+import { faPlus, faEdit, faBars } from "@fortawesome/free-solid-svg-icons";
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+library.add(faEdit, faPlus, faBars);
+
+import plausible from "./plugins/plausible";
+
+const plausibleOptions = {
+  domain: "www.pwa-template.de",
+  apiHost: "https://tracking.jmartz.gmbh",
   hashMode: false,
   trackLocalhost: false,
 };
 
 const app = createApp(App);
 
-app.use(plausible,plausibleOptions);
+app.use(plausible, plausibleOptions);
+app.component("fa", FontAwesomeIcon);
 app.use(routes);
 app.use(store);
-app.mount('#app')
+app.mount("#app");
