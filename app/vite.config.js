40b28c82fe79b9438a7fcd5c8610c2d815488210
Jonathan Martz <git@jmartz.de>
2022-11-23 11:59:40 +0100

    fix servie worker cache

--- app/vite.config.js
+++ app/vite.config.js
@@ -5,7 +5,11 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(),VitePWA()],
+  plugins: [vue(),VitePWA({
+    workbox: {
+      cleanupOutdatedCaches: true,
+    },
+  })],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
